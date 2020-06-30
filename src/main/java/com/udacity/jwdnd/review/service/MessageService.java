c24a1e7f129de1f861d4744d7ec22b18a2df291d
Marek Ciesla <ciesla.marek@gmail.com>
2020-06-30 13:33:19 +0200

    feat: created MessageService class

--- src/main/java/com/udacity/jwdnd/review/service/MessageService.java
+++ src/main/java/com/udacity/jwdnd/review/service/MessageService.java
@@ -1,4 +1,41 @@
 package com.udacity.jwdnd.review.service;
 
+import com.udacity.jwdnd.review.model.ChatForm;
+import com.udacity.jwdnd.review.model.ChatMessage;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class MessageService {
+    private List<ChatMessage> chatMessages;
+
+    @PostConstruct
+    public void postConstructor() {
+        System.out.println("Creating Message Service bean");
+        this.chatMessages = new ArrayList<>();
+    }
+
+    public void addMessage(ChatForm chatForm){
+        ChatMessage newMessage = new ChatMessage();
+        newMessage.setUsername(chatForm.getUsername());
+        switch (chatForm.getMessageType()){
+            case "Say":
+                newMessage.setMessage(chatForm.getMessageText());
+                break;
+            case "Shout":
+                newMessage.setMessage(chatForm.getMessageText().toUpperCase());
+                break;
+            case "Whisper":
+                newMessage.setMessage(chatForm.getMessageText().toLowerCase());
+                break;
+        }
+        this.chatMessages.add(newMessage);
+    }
+
+    public List<ChatMessage> getChatMessages() {
+        return chatMessages;
+    }
 }
